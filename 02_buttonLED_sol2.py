adfe1f53f4acdaa0455c838a3b74f8b198d48674
Hana Kim <hsk003@eng.ucsd.edu>
2019-08-26 15:58:20 -0700

    Our button was broken

---
+++ 02_buttonLED_sol2.py
@@ -0,0 +1,60 @@
+# This program demonstrates how to read a button
+# It will use this information to control an LED
+
+# Import the relevant libraries
+import RPi.GPIO as GPIO
+import time
+
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BOARD)
+
+# Set GPIO Pins
+LedPin = 11                                     # GPIO pin for the LED
+BtnPin = 15                                     # GPIO pin for the button
+
+# Set GPIO direction (IN / OUT)
+GPIO.setup(LedPin, GPIO.OUT)                            # Set LedPin's mode to output
+GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # Set BtnPin's mode is input, and pull up to high level (3.3V)
+
+# Start conditions
+GPIO.output(LedPin, GPIO.LOW)                   # Set LedPin low to turn the led off 
+led = 0
+
+
+pressed = 0
+
+def toggle():
+    global led
+    if led == 0:
+        GPIO.output(LedPin, GPIO.HIGH)
+        led = 1
+    else:
+        GPIO.output(LedPin, GPIO.LOW)
+        led = 0
+
+
+
+# Main program 
+if __name__ == '__main__':                      # Program starts here
+
+    try:
+        # This code repeats forever
+        while True:
+            buttonState = GPIO.input(BtnPin)
+
+            print("Btn: " + str(buttonState))
+            print("pressed: " + str(pressed))
+
+            if buttonState==0:
+                if pressed == 0: 
+                    toggle()
+                pressed = 1
+            else:
+                pressed = 0
+
+    # Reset by pressing CTRL + C
+    except KeyboardInterrupt:              
+        print("Program stopped by User")
+        GPIO.output(LedPin, GPIO.LOW)          	# LED off
+        GPIO.cleanup()                          # Release resource
+
